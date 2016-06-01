c7209f97931a0828c7d3118fb0a9a8301eaa7661
Neo Hsu <NeoHsuDroid@gmail.com>
2016-06-01 11:00:18 +0800

    refactor(algorithms): Change return handling for "Longest Common Prefix"

--- algorithms/longestCommonPrefix/longestCommonPrefix.go
+++ algorithms/longestCommonPrefix/longestCommonPrefix.go
@@ -2,17 +2,19 @@ package longestCommonPrefix
 
 // 取第一個 array element 一個個字元和其他 element 做比對
 
-func longestCommonPrefix(strs []string) string {
+func longestCommonPrefix(strs []string) (str string) {
 	if len(strs) == 0 {
-		return ""
+		return
 	}
 
 	for i := 0; i < len(strs[0]); i++ {
 		for j := 0; j < len(strs); j++ {
 			if i >= len(strs[j]) || strs[0][i] != strs[j][i] {
-				return strs[0][:i]
+				str = strs[0][:i]
+				return
 			}
 		}
 	}
-	return strs[0]
+	str = strs[0]
+	return
 }
